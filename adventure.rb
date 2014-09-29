66611885264f6ba1aa475a33e69ee5a365736b95
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-09-29 13:07:50 +0100

    add show inventory and quit - lacks error checking

--- adventure.rb
+++ adventure.rb
@@ -6,6 +6,7 @@ def user_input	# returns verb, noun
 	puts "What would you like to do? Please enter two word command"
 	print "> "
 	@input_array = gets.chomp.split(' ')
+	@verb, @noun = 'default', 'default'  # might be better off with nil as default values
 	if @input_array.count == 2
 		@verb, @noun = @input_array[0], @input_array[1] # this does not look very neat
 	else
@@ -26,22 +27,32 @@ current_room = my_dungeon.rooms[1]
 current_room.describe
 
 #test dungeon
-verb, noun = user_input
-puts "verb is #{verb}"
-puts "noun is #{noun}"
+(0..5).each do #test loop few times
+	current_room.describe
+	puts ''
+	verb, noun = user_input
+	puts "verb is #{verb}, noun is #{noun}" # for debugging
 	case 
-	when verb == 'go'
-		current_room = current_room.move noun	
-	when verb == 'get' # get currently picks up random object without noun
-		inventory.add_item current_room.get_item
-	when verb == 'drop' # drop currently drops random object from inventory
-		current_room.add_item inventory.get_item
-	when verb == 'show'
-		inventory.describe if noun = 'inventory'
-	else	
-		puts "sorry, didn't understand"		
+		when verb == 'go'
+			current_room = current_room.move noun
+			puts "you went #{noun}"	
+		when verb == 'get' # get currently picks up random object without noun
+			inventory.add_item current_room.get_item
+			puts 'you picked something up'
+		when verb == 'drop' # drop currently drops random object from inventory
+			current_room.add_item inventory.get_item
+			puts 'you dropped something'
+		when verb == 'show'
+			inventory.describe if noun == 'inventory' 
+		when verb = 'quit'
+			puts 'bye'
+			break	
+		else	
+			puts "sorry, didn't understand"		
 	end
-current_room.describe
+	
+end
+
 
 
 #current_room.get_item(0)
