6da59134d6adefc22cf28347d01740cc3455dc9b
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-09-29 13:40:42 +0100

    improve and test data second dungeon

--- dungeon_setup.rb
+++ dungeon_setup.rb
@@ -6,7 +6,7 @@ def add_data (dungeon)
 	dungeon.add_room Room.new 'A cluttered kitchen'
 	dungeon.add_room Room.new 'A room with no exits'
 
-		# i don't like the way we add exits - is there a better way?
+	# i don't like the way we add exits - is there a better way?
 	dungeon.rooms[1].add_exit 'N', dungeon.rooms[2]
 	dungeon.rooms[2].add_exit 'S', dungeon.rooms[1]
 	dungeon.rooms[1].add_exit 'W', dungeon.rooms[3]
@@ -19,13 +19,15 @@ def add_data (dungeon)
 end
 
 def add_data2 (dungeon)
-
+	#setup for makers academy dungeon - not yet used
 	#set up rooms
 	dungeon.add_room Room.new 'Street outside office'
-	dungeon.add_room Room.new 'Lobby'
+	dungeon.add_room Room.new 'The lobby of 25 city road'
 	dungeon.add_room Room.new 'Lift, ground floor'
 	dungeon.add_room Room.new 'Lift, floor 4'
-	
+	dungeon.add_room Room.new 'Landing, floor 4'
+	dungeon.add_room Room.new 'Reception of Makers Academy'
+
 
 
 		# i don't like the way we add exits - is there a better way?
@@ -35,10 +37,15 @@ def add_data2 (dungeon)
 	dungeon.rooms[3].add_exit 'W', dungeon.rooms[2]
 	dungeon.rooms[3].add_exit 'U', dungeon.rooms[4]
 	dungeon.rooms[4].add_exit 'D', dungeon.rooms[3]
+	dungeon.rooms[4].add_exit 'W', dungeon.rooms[5]
+	dungeon.rooms[5].add_exit 'E', dungeon.rooms[4]
+	dungeon.rooms[5].add_exit 'W', dungeon.rooms[6]
+	dungeon.rooms[6].add_exit 'E', dungeon.rooms[5]
 
 	#add items
-	dungeon.rooms[2].add_item Dungeon_item.new 'a cigarette end' , 100
-	dungeon.rooms[2].add_item Dungeon_item.new 'a sleeping security guard' , 100
+	dungeon.rooms[1].add_item Dungeon_item.new 'a cigarette end' , 1
+	dungeon.rooms[2].add_item Dungeon_item.new 'a security guard' , 100
+	dungeon.rooms[6].add_item Dungeon_item.new 'a sticker' , 2
 	dungeon.rooms[0].add_item Dungeon_item.new 'a boiled sweet covered in pocket fluff', 1
 	#my_dungeon.inventory.add_item Dungeon_item.new 'a boiled sweet covered in pocket fluff', 1 #this doesn't work - why?
 end
