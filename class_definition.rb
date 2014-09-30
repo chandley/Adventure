660c2aa8873475fdeb66ddfe4529766f5d905904
Chris Handley <github.20.alabama3@spamgourmet.com>
2014-09-30 14:39:56 +0100

    Improved user input handling, tidy up code

--- class_definition.rb
+++ class_definition.rb
@@ -2,8 +2,7 @@
 class Dungeon_thing  
 	attr_accessor :description, :long_description
 	def initialize (description, long_description)
-		@description = description
-		@long_description = long_description # default value
+		@description, @long_description = description, long_description
 	end
 end
 
@@ -12,16 +11,14 @@ class Room < Dungeon_thing
 	def initialize (description, long_description)
 		super description, long_description
 		@exits = {}	
-		@items = []
-		@monsters = []
+		@items, @monsters = [],[]
 	end
 
-	def long_description #override accessor to show exits
-		return_string = ''
-		return_string << @long_description + "\n"
-		@exits.each {|direction,room| return_string <<"there is an exit #{direction.upcase} to #{room.description}\n" } unless @exits.keys.count == 0
+	def long_description #override accessor to show exits, items, monsters
+		return_string = @long_description + "\n"
+		@exits.each {|direction,room| return_string <<"there is an exit #{direction.upcase} to #{room.description}\n" } #unless @exits.keys.count == 0
 		@items.each {|item|  return_string << "you see item #{item.description}\n"}
-		@monsters.each {|monster| return_string << "you see monster #{monster.description}\n"}
+		@monsters.each  {|monster|return_string << "you see monster #{monster.status} #{monster.description}\n"} 
 		return return_string
 	end
 end
@@ -30,51 +27,39 @@ class Item < Dungeon_thing
 end
 
 class Monster < Dungeon_thing
-	attr_accessor :alive	
+	attr_accessor :status	
 	def initialize (description, long_description)
 		super description, long_description
-		@alive = true	# monster starts off alive..
+		@status = "angry"	# monster starts off alive..
 	end
 end
 
-#### functions
-def user_input (prompt)# returns verb, noun
-	# this could use some refactoring
+def user_input (prompt)
 	puts prompt
-	print "> "
-	@input_array = gets.chomp.split(' ')
-	@verb, @noun = 'default', 'default'  # might be better off with nil as default values
-	if @input_array.count == 2
-		@verb, @noun = @input_array[0], @input_array[1] # this does not look very neat
-	else
-		puts "Sorry, didn't understand"
-	end
-	return @verb.downcase, @noun.downcase
+	until (input_array = gets.chomp.split(' ')).count > 0 do end # get some input
+	return input_array.first.downcase, input_array.last.downcase
 end
 
 ###### setup dungeon
-kitchen = Room.new 'kitchen', 'A cluttered kitchen, light streams in through the window'
-dining = Room.new 'dining room',  'A dusty dining room. A fly buzzes around'
+kitchen = Room.new 'kitchen', 'a cluttered kitchen, light streams in through the window.'
+dining = Room.new 'dining room',  'a dusty dining room. A fly buzzes around.'
 inventory = Room.new 'inventory', 'You are carrying the following'
-
 kitchen.exits['N'] = dining
 dining.exits['S'] = kitchen
 
+# setup items
 toast = Item.new 'toast', 'A piece of buttered toast'
-hobgoblin = Monster.new 'Hobgoblin', 'A nasty looking goblin'
-
 empty_gun = Item.new 'unloaded gun','an automatic pistol, it contains no bullets'
 bullets = Item.new 'some bullets', 'some bullets. these look as if they will fit in the gun'
-loaded_gun = Item.new 'loaded gun', 'a gun loaded with bullets'
-
+loaded_gun = Item.new 'loaded gun', 'a gun loaded with bullets'	# not initially in any room
 kitchen.items.push toast
-kitchen.monsters.push hobgoblin
-
 dining.items.push empty_gun
 dining.items.push bullets
+# setup monsters
+hobgoblin = Monster.new 'Hobgoblin', 'A nasty looking goblin'
+kitchen.monsters.push hobgoblin
 
 ##### test dungeon
-
 current_room = kitchen	
 
 loop do 
@@ -87,7 +72,7 @@ loop do
 	  	current_room = current_room.exits[noun.upcase[0]] # use first character only
 	  	puts "You go #{noun.upcase} to #{current_room.description}"
 	when  'inventory','inv'
-	  	inventory.long_description
+	  	puts inventory.long_description
 	when  'get' 	
 	  	item = inventory.items.push current_room.items.pop
 	  	puts "you pick up #{item.last.description}"
@@ -99,23 +84,26 @@ loop do
 			inventory.items.delete(empty_gun)
 			inventory.items.delete(bullets)
 			inventory.items.push loaded_gun
-			puts "CLICK! You loaded the bullets into the gun"
+			puts "click! You loaded the bullets into the gun"
 		else
-			puts "You need gun and bullets to load"
+			puts "You need empty gun and bullets to load"
 		end
 	when 'shoot'
 		if inventory.items.include?(loaded_gun) && current_room.monsters.count > 0
-			puts "bang bang"
+			puts "BANG BANG"
 			current_room.monsters.each do |monster|
 				puts "You hit #{monster.description}"
-				monster.alive = false
-				monster.description = "dead " + monster.description
+				monster.status = "dead"
 				monster.long_description = "A bloodstained body with bullet holes"
 			end
 		else
 			puts "you can't shoot anything"
 		end
-
+	when 'exit','quit','q'
+		puts 'thanks for playing, bye'
+		break
+	else
+		puts "Sorry, didn't understand. Try 'go N', 'inv', 'get item' or 'quit'"
   	end	
 
 end
